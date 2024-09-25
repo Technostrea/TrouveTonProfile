96dfaf7a914573653a4179ea17fcba3e3a988d48
Styve Lioumba <37368019+StyveLioumba@users.noreply.github.com>
2024-09-25 18:25:50 +0200

    la generation et telechargement du qrcode pret (#3)
    Co-authored-by: styve Lioumba <styve.lioumba@jdc.fr>

--- src/app/routes/profile/profile-detail/profile-detail.component.ts
+++ src/app/routes/profile/profile-detail/profile-detail.component.ts
@@ -1,19 +1,31 @@
 import {Component, inject} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
+import {QRCodeModule} from "angularx-qrcode";
+import {SafeUrl} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-profile-detail',
   standalone: true,
-  imports: [],
+  imports: [
+    QRCodeModule
+  ],
   templateUrl: './profile-detail.component.html',
   styleUrl: './profile-detail.component.scss'
 })
 export class ProfileDetailComponent {
 
   private readonly route = inject(ActivatedRoute);
+  private readonly router = inject(Router);
   protected profile = this.route.snapshot.data['profile'];
+
+  protected myAngularxQrCode: string = "http://loalhost:4200";
+  protected qrCodeDownloadLink: SafeUrl = "";
 
   constructor() {
     console.log(this.profile)
+    this.myAngularxQrCode = this.myAngularxQrCode + this.router.url;
+  }
+  onChangeURL(url: SafeUrl) {
+    this.qrCodeDownloadLink = url;
   }
 }
